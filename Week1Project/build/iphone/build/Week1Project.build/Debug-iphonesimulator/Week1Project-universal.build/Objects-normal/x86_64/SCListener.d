d78d1aefce416fae3695d0ca426559b887599fba
Allen Boynton <aboynton@fullsail.edu>
2015-08-09 15:21:24 -0400

    Update folders

---
+++ Week1Project/build/iphone/build/Week1Project.build/Debug-iphonesimulator/Week1Project-universal.build/Objects-normal/x86_64/SCListener.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/SCListener.m \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/SCListener.h
---
+++ Week1Project/build/iphone/build/Week1Project.build/Debug-iphonesimulator/Week1Project-universal.build/Objects-normal/x86_64/TiStylesheet.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/TiStylesheet.m \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/TiStylesheet.h \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/TiBase.h \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/TiThreading.h \
+  /Users/aboynton/Desktop/AVF1508/Week1Project/build/iphone/Classes/TiPublicAPI.h
