9e02bb32b735d35cbed7f68308a082c404cd1996
Allen Boynton <aboynton@fullsail.edu>
2015-08-26 19:10:16 -0400

    Checking all cross platfroms
    Works on my iPad, Note 3 and iPhone 6. Project 2.

--- Week2Project/Resources/app.js
+++ Week2Project/Resources/app.js
@@ -1,19 +1,25 @@
 // Allen Boynton
 // AVF 1508
 // Week 2 Project : Storage/SQLite
-// August 16, 2015
+// August 16, 2015  ***Revised***
 
 // Set background
 Titanium.UI.setBackgroundColor("#000");
 
-var win = Ti.UI.createWindow({
-	backgroundColor: "gray",
-	layout: "vertical"
-});
-
-var ui = require("ui");
-var api = require("api");
-var geo = require("geo");
-var storage = require("storage");
-
-win.open();
+// Network connectivity check
+if (Ti.Network.online){
+	Ti.API.info("online");
+	
+	//Required modules
+	var ui = require("ui");
+	var api = require("api");
+	var geo = require("geo");
+	geo.getGeo();
+	
+// If no network present, revert to local storage
+} else {
+	alert ("No network connection detected. Connect to network in Settings or tap OK to use last information received.");
+	var storage = require("storage");
+	Ti.API.info("No connection");
+	storage.read(); 
+};
