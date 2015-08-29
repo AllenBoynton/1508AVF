49f900f0ff0ba5df9d2ab821b8a1992b7725b8c7
Allen Boynton <aboynton@fullsail.edu>
2015-08-28 22:36:52 -0400

    Transferred resources
    Transposed files and info into Project 4 from the continuation of
    Project 3.

---
+++ Week4Project/Resources/geo.js
@@ -0,0 +1,20 @@
+// Gets geolocation
+var api = require("api");
+
+var getGeo = function(){
+	Ti.Geolocation.purpose = "This application requires your Current Location.";
+	Ti.Geolocation.getCurrentPosition(function(e){
+		if(Ti.Platform.osname =="android"){
+			var latitude = 34.8621210;
+			var longitude = -82.3539020;
+			api.network(latitude, longitude);
+		} else {
+			var latitude = e.coords.latitude;
+			var longitude = e.coords.longitude;
+			api.network(latitude, longitude);
+		}
+	});
+};
+
+// Exports geolocation function 
+exports.getGeo = getGeo;
