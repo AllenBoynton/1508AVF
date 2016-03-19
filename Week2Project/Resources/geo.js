4435f0ac4a7890343bbe4dfb12f62c4291d1b281
Allen Boynton <AllenBoynton@users.noreply.github.com>
2016-03-19 13:02:49 -0400

    adjusted syntax

--- Week2Project/Resources/geo.js
+++ Week2Project/Resources/geo.js
@@ -2,13 +2,13 @@
 var getGeo = function(){
 	Ti.Geolocation.purpose = "Current location is needed to get your local weather.";
 		if(Ti.Platform.osname === "android") {
-			var latitude = 34.8621210;
+			var latitude  = 34.8621210;
 			var longitude = -82.3539020;
 			var url = "http://api.wunderground.com/api/d2e13383813c88c5/conditions/alert/almanac/q/" + latitude + "," + longitude + ".json";
 			api.network(url);
 		} else {
 			Ti.Geolocation.getCurrentPosition(function(e){
-				var latitude = e.coords.latitude;
+				var latitude  = e.coords.latitude;
 				var longitude = e.coords.longitude;
 				var url = "http://api.wunderground.com/api/d2e13383813c88c5/conditions/alert/almanac/q/" + latitude + "," + longitude + ".json";
 				api.network(url);
