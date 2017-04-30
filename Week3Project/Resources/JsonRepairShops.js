09cdd4671e03a3b6067bf228bce73be54298e3fb
Allen Boynton <AllenBoynton@users.noreply.github.com>
2017-04-29 22:20:27 -0400

    Create JsonRepairShops.js
    Added for easy reference

---
+++ Week3Project/Resources/JsonRepairShops.js
@@ -0,0 +1,339 @@
+repairShops({
+   "repairshops":[
+      {
+         "operations":{
+            "Wednesday":"7:30 AM-6:00 PM",
+            "Tuesday":"7:30 AM-6:00 PM",
+            "Thursday":"7:30 AM-6:00 PM",
+            "Saturday":"8:00 AM-5:00 PM",
+            "Friday":"7:30 AM-6:00 PM",
+            "Monday":"7:30 AM-6:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16146",
+         "name":"Breakaway Honda",
+         "niceName":"BreakawayHonda56102",
+         "distance":0.2663840878195881,
+         "address":{
+            "street":"330 Woodruff Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.83312,
+            "longitude":-82.34211,
+            "zipcode":"29607"
+         },
+         "repairshopId":"127356",
+         "make":{
+            "name":"Honda",
+            "niceName":"honda"
+         },
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"07:30 AM-06:00 PM",
+            "Tuesday":"07:30 AM-06:00 PM",
+            "Thursday":"07:30 AM-06:00 PM",
+            "Saturday":"08:00 AM-05:00 PM",
+            "Friday":"07:30 AM-06:00 PM",
+            "Monday":"07:30 AM-06:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16146",
+         "name":"Breakaway Honda",
+         "niceName":"BreakawayHonda754709",
+         "distance":0.2663840878195881,
+         "address":{
+            "street":"330 Woodruff Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.83312,
+            "longitude":-82.34211,
+            "zipcode":"29607"
+         },
+         "repairshopId":"809089",
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"7:30 AM-6:00 PM",
+            "Tuesday":"7:30 AM-6:00 PM",
+            "Thursday":"7:30 AM-6:00 PM",
+            "Saturday":"8:00 AM-1:00 PM",
+            "Friday":"7:30 AM-6:00 PM",
+            "Monday":"7:30 AM-6:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16144",
+         "name":"Bradshaw Acura",
+         "niceName":"BradshawAcura56100",
+         "distance":0.38407336911209733,
+         "address":{
+            "street":"2450 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.827187,
+            "longitude":-82.34614,
+            "zipcode":"29607"
+         },
+         "repairshopId":"127352",
+         "make":{
+            "name":"Acura",
+            "niceName":"acura"
+         },
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"07:30 AM-06:00 PM",
+            "Tuesday":"07:30 AM-06:00 PM",
+            "Thursday":"07:30 AM-06:00 PM",
+            "Saturday":"08:00 AM-01:00 PM",
+            "Friday":"07:30 AM-06:00 PM",
+            "Monday":"07:30 AM-06:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16144",
+         "name":"Bradshaw Acura",
+         "niceName":"BradshawAcura754707",
+         "distance":0.38407336911209733,
+         "address":{
+            "street":"2450 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.827187,
+            "longitude":-82.34614,
+            "zipcode":"29607"
+         },
+         "repairshopId":"802750",
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"7:30 AM-6:00 PM",
+            "Tuesday":"7:30 AM-6:00 PM",
+            "Thursday":"7:30 AM-6:00 PM",
+            "Saturday":"Day off",
+            "Friday":"7:30 AM-6:00 PM",
+            "Monday":"7:30 AM-6:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16145",
+         "name":"Bradshaw INFINITI",
+         "niceName":"BradshawINFINITI_56101",
+         "distance":0.38458007885881684,
+         "address":{
+            "street":"2448 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.82726,
+            "longitude":-82.346193,
+            "zipcode":"29607"
+         },
+         "repairshopId":"127354",
+         "make":{
+            "name":"INFINITI",
+            "niceName":"infiniti"
+         },
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"07:30 AM-06:00 PM",
+            "Tuesday":"07:30 AM-06:00 PM",
+            "Thursday":"07:30 AM-06:00 PM",
+            "Saturday":"Day off",
+            "Friday":"07:30 AM-06:00 PM",
+            "Monday":"07:30 AM-06:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16145",
+         "name":"Bradshaw INFINITI",
+         "niceName":"BradshawINFINITI_754708",
+         "distance":0.38458007885881684,
+         "address":{
+            "street":"2448 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.82726,
+            "longitude":-82.346193,
+            "zipcode":"29607"
+         },
+         "repairshopId":"794837",
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"7:30 AM-6:00 PM",
+            "Tuesday":"7:30 AM-6:00 PM",
+            "Thursday":"7:30 AM-6:00 PM",
+            "Saturday":"Day off",
+            "Friday":"7:30 AM-6:00 PM",
+            "Monday":"7:30 AM-6:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16147",
+         "name":"Carlton Motorcars",
+         "niceName":"CarltonMotorcars56103",
+         "distance":0.38514694548212386,
+         "address":{
+            "street":"2446 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.827334,
+            "longitude":-82.346246,
+            "zipcode":"29607"
+         },
+         "repairshopId":"127358",
+         "make":{
+            "name":"Mercedes-Benz",
+            "niceName":"mercedes-benz"
+         },
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"07:30 AM-06:00 PM",
+            "Tuesday":"07:30 AM-06:00 PM",
+            "Thursday":"07:30 AM-06:00 PM",
+            "Saturday":"Day off",
+            "Friday":"07:30 AM-06:00 PM",
+            "Monday":"07:30 AM-06:00 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16147",
+         "name":"Carlton Motorcars",
+         "niceName":"CarltonMotorcars754710",
+         "distance":0.38514694548212386,
+         "address":{
+            "street":"2446 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.827334,
+            "longitude":-82.346246,
+            "zipcode":"29607"
+         },
+         "repairshopId":"774674",
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"07:30 AM-05:30 PM",
+            "Tuesday":"07:30 AM-05:30 PM",
+            "Thursday":"07:30 AM-05:30 PM",
+            "Saturday":"Day off",
+            "Friday":"07:30 AM-05:30 PM",
+            "Monday":"07:30 AM-05:30 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16154",
+         "name":"John Finger Mazda",
+         "niceName":"JohnFingerMazda754717",
+         "distance":0.39157311858855365,
+         "address":{
+            "street":"2512 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.82574,
+            "longitude":-82.345068,
+            "zipcode":"29607"
+         },
+         "repairshopId":"794118",
+         "type":"REPAIRSHOP"
+      },
+      {
+         "operations":{
+            "Wednesday":"7:30 AM-5:30 PM",
+            "Tuesday":"7:30 AM-5:30 PM",
+            "Thursday":"7:30 AM-5:30 PM",
+            "Saturday":"Day off",
+            "Friday":"7:30 AM-5:30 PM",
+            "Monday":"7:30 AM-5:30 PM",
+            "Sunday":"Day off"
+         },
+         "active":true,
+         "dealerId":"16154",
+         "name":"John Finger Mazda",
+         "niceName":"JohnFingerMazda56118",
+         "distance":0.39157311858855365,
+         "address":{
+            "street":"2512 Laurens Rd",
+            "apartment":"",
+            "city":"Greenville",
+            "stateCode":"SC",
+            "stateName":"South Carolina",
+            "county":"Greenville",
+            "country":"USA",
+            "latitude":34.82574,
+            "longitude":-82.345068,
+            "zipcode":"29607"
+         },
+         "repairshopId":"118046",
+         "make":{
+            "name":"Mazda",
+            "niceName":"mazda"
+         },
+         "type":"REPAIRSHOP"
+      }
+   ],
+   "repairshopsCount":1053,
+   "links":[
+      {
+         "rel":"first",
+         "href":"/api/dealer/v2/repairshops?sortby=distance:ASC&api_key=a69s88jdn9qtfdyufxr9mch9&zipcode=29607&callback=repairShops&radius=100&view=basic&pagesize=10&pagenum=1"
+      },
+      {
+         "rel":"next",
+         "href":"/api/dealer/v2/repairshops?sortby=distance:ASC&api_key=a69s88jdn9qtfdyufxr9mch9&zipcode=29607&callback=repairShops&radius=100&view=basic&pagesize=10&pagenum=2"
+      },
+      {
+         "rel":"last",
+         "href":"/api/dealer/v2/repairshops?sortby=distance:ASC&api_key=a69s88jdn9qtfdyufxr9mch9&zipcode=29607&callback=repairShops&radius=100&view=basic&pagesize=10&pagenum=106"
+      }
+   ]
+})
